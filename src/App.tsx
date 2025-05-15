57f18d79bff25fc0999119733f63740f3c92e3cf
AndriySt92 <andriyua-1992@mail.ru>
2025-05-15 21:43:27 +0300

    feat: add React Router. Set up basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import { Layout } from './components';
+import { Catalog, Contacts, Home, Products } from './pages';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/products/:category" element={<Products />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/Categories/:category" element={<Catalog />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
