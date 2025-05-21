f272134895541814f5878344993beb83601ecaa2
AndriySt92 <andriyua-1992@mail.ru>
2025-05-21 22:30:12 +0300

    feat(Layout): add scroll-to-top on route change, update styles

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,19 +1,28 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 
-import Header from './header/Header';
-import Footer from './Footer';
-import ScrollToTopButton from './ScrollToTopButton';
-import SocialMediaButton from './SocialMediaButton';
+import { Footer, Header, ScrollToTopButton, SocialMediaButton } from './';
 
 const Layout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }, [pathname]);
+
   return (
     <div className="relative z-50 flex min-h-screen flex-col">
       <Header />
-      <main className="sm:mb:20 mb-14 mt-[300px] flex-1 lg:mt-44">
+      <main className="sm:mb:20 mt-[248px] flex-1 pb-12 sm:pb-16 lg:mt-[152px] lg:pb-20 xl:mt-[156px]">
         <Outlet />
       </main>
-      <ScrollToTopButton />
-      <SocialMediaButton />
+      <div className="fixed bottom-2 right-4 z-50 flex flex-col justify-center gap-2 sm:bottom-2">
+        <SocialMediaButton />
+        <ScrollToTopButton />
+      </div>
       <Footer />
     </div>
   );
